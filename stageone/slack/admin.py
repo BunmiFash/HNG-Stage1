0456dc38367aa02ca7cb0d5a7a1836d9e9ebd4e3
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-09-09 21:31:34 +0100

    empty

--- stageone/slack/admin.py
+++
@@ -1,8 +0,0 @@
-"""
-A module that registers the class as
-a table in the database
-"""
-from django.contrib import admin
-from slack.models import SlackDetails
-
-admin.site.register(SlackDetails)
--- stageone/slack/urls.py
+++
@@ -1,9 +0,0 @@
-"""
-A module that defines the endpoints
-"""
-from django.urls import path
-from slack.views import Details
-
-urlpatterns = [
-        path('details/', Details.as_view()),
-]
